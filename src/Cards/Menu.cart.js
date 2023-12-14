c53ceefc7d4be0ad76bb031ad319c91a2b966494
NwobiaDavid <dnwobia@gmail.com>
2023-12-14 19:36:51 +0100

    added the edit cart option

--- src/Cards/Menu.cart.js
+++ src/Cards/Menu.cart.js
@@ -1,49 +1,53 @@
 const mongoose = require('mongoose');
-const {Telegraf, Markup } = require('telegraf');
+const { Telegraf, Markup } = require('telegraf');
 const { MenuItem } = require('../../models');
 
 let botx;
+let currentItemIndex;
+
 async function manageCart(ctx, bot, existingCarts, userCarts) {
     botx = bot;
-        const userId = ctx.from.id;
-        const existingCart = existingCarts.get(userId) || {};
-    
-        if (Object.keys(existingCart).length > 0) {
-          const itemDetails = await Promise.all(
+    currentItemIndex = 0;
+
+    const userId = ctx.from.id;
+    const existingCart = existingCarts.get(userId) || {};
+
+    if (Object.keys(existingCart).length > 0) {
+        const itemDetails = await Promise.all(
             Object.keys(existingCart).map(async (itemId) => {
-              const item = await MenuItem.findById(itemId);
-              return item
-                ? { name: item.itemName, quantity: existingCart[itemId] }
-                : null;
+                const item = await MenuItem.findById(itemId);
+                return item
+                    ? { name: item.itemName, quantity: existingCart[itemId] }
+                    : null;
             })
-          );
-    
-          // Filter out items that couldn't be found
-          const validItemDetails = itemDetails.filter((item) => item !== null);
-    
-          if (validItemDetails.length > 0) {
+        );
+
+        // Filter out items that couldn't be found
+        const validItemDetails = itemDetails.filter((item) => item !== null);
+
+        if (validItemDetails.length > 0) {
             const cartMessage = validItemDetails
-              .map((item) => `${item.name}: ${item.quantity}`)
-              .join('\n');
-    
+                .map((item) => `${item.name}: ${item.quantity}`)
+                .join('\n');
+
             const keyboard = Markup.inlineKeyboard([
-              [Markup.button.callback('edit cart', 'edit_cart'), Markup.button.callback('Checkout', 'checkout')],
-              [Markup.button.callback('back to home', 'browsing_categories')],
+                [Markup.button.callback('edit cart', 'edit_cart'), Markup.button.callback('Checkout', 'checkout')],
+                [Markup.button.callback('back to home', 'browsing_categories')],
             ]);
-    
+
             ctx.editMessageText(`Your Cart:\n${cartMessage}`, keyboard);
-          } else {
-            ctx.reply('Some items in your cart could not be found.');
-          }
-    
-          bot.action('edit_cart', async (ctx) => {
-            // Call the editCart function to handle the 'edit_cart' callback
-            await editCart(ctx, userCarts, existingCarts,bot);
-          });
-          
         } else {
-          ctx.reply('Your cart is empty.');
+            ctx.reply('Some items in your cart could not be found.');
         }
+
+        bot.action('edit_cart', async (ctx) => {
+            // Call the editCart function to handle the 'edit_cart' callback
+            await editCart(ctx, userCarts, existingCarts, bot);
+        });
+
+    } else {
+        ctx.reply('Your cart is empty.');
+    }
 }
 
 async function editCart(ctx, userCarts, existingCarts, bot) {
@@ -72,42 +76,23 @@ async function editCart(ctx, userCarts, existingCarts, bot) {
 
         if (itemsInCart.length > 0) {
             // Construct a message with buttons for editing each item in the cart
-            
+            const cartEditMessage = `Editing: ${itemsInCart[currentItemIndex].name}: ${itemsInCart[currentItemIndex].existingQuantity}`;
 
-            // Register button callbacks for each item in the cart
-            itemsInCart.map((item) => {
-                const cartEditMessage = `editing: ${item.name}: ${item.existingQuantity}`;
-                // console.log(item.id)
-                const keyboard = Markup.inlineKeyboard([
-                    [Markup.button.callback('Increase Quantity', `increase_quantity_${item.id}`), Markup.button.callback('Decrease Quantity', `decrease_quantity_${item.id}`)],
-                    [Markup.button.callback('Remove Item', `remove_item_${item.id}`)],
-                    [Markup.button.callback('Back to Cart', 'manage_cart')],
-                ]);
-    
-                ctx.editMessageText(`Edit Cart:\n${cartEditMessage}`, keyboard);
-                
-                // registerItemCallbacks(ctx, userCarts, existingCarts, item, bot);
-            });
+            const keyboard = Markup.inlineKeyboard([
+                [Markup.button.callback('Increase Quantity', `increase_quantity_${itemsInCart[currentItemIndex].id}`), Markup.button.callback('Decrease Quantity', `decrease_quantity_${itemsInCart[currentItemIndex].id}`)],
+                [Markup.button.callback('Remove Item', `remove_item_${itemsInCart[currentItemIndex].id}`)],
+                [Markup.button.callback('Back to Cart', 'manage_cart')],
+                [Markup.button.callback('Previous', 'previous_item'), Markup.button.callback('Next', 'next_item')],
+                [Markup.button.callback('Back to home', 'browsing_categories')],
+            ]);
+
+            ctx.editMessageText(`Edit Cart:\n${cartEditMessage}`, keyboard);
+
+            // Register actions for the current item, passing itemsInCart
+            registerItemCallbacks(ctx, userCarts, existingCarts, itemsInCart[currentItemIndex], bot, itemsInCart);
         } else {
             ctx.reply('Your cart is empty.');
         }
-
-        
-console.log("this is botx==>", botx);
-        botx.action(/increase_quantity_(.+)/, (ctx) => {
-            // quantity = await updateUserCart(user, itemId, -1, userCarts, ctx);
-            const itemId = ctx.match[1];
-            updateCartItemQuantity(ctx,userCarts, existingCarts, itemId, 1);
-        });
-    
-        botx.action(/decrease_quantity_(.+)/, (ctx) => {
-            const itemId = ctx.match[1];
-            updateCartItemQuantity(ctx, userCarts, existingCarts, itemId, -1);
-        });
-    
-        botx.action(/remove_item_(.+)/, (ctx) => {
-            removeCartItem(ctx, userCarts, existingCarts, id);
-        });
 
     } catch (error) {
         console.error('Error editing cart:', error);
@@ -115,26 +100,37 @@ console.log("this is botx==>", botx);
     }
 }
 
-// async function registerItemCallbacks(ctx, userCarts, existingCarts, item, bot) {
-//     // const { id, existingQuantity, userQuantity } = item;
-//     console.log("this is the bot====>", bot)
-//     bot.action(/increase_quantity_(.+)/, async (ctx) => {
-//         // quantity = await updateUserCart(user, itemId, -1, userCarts, ctx);
-//         const itemId = ctx.match[1];
-//         await updateCartItemQuantity(ctx,userCarts, existingCarts, itemId, 1);
-//     });
+async function registerItemCallbacks(ctx, userCarts, existingCarts, item, bot, itemsInCart) {
+    const { id, existingQuantity, userQuantity } = item;
 
-//     bot.action(/decrease_quantity_(.+)/, async (ctx) => {
-//         const itemId = ctx.match[1];
-//         await updateCartItemQuantity(ctx, userCarts, existingCarts, itemId, -1);
-//     });
+    botx.action(/increase_quantity_(.+)/, (ctx) => {
+        const itemId = ctx.match[1];
+        updateCartItemQuantity(ctx, userCarts, existingCarts, itemId, 1, bot);
+    });
 
-//     bot.action(/remove_item_(.+)/, async (ctx) => {
-//         await removeCartItem(ctx, userCarts, existingCarts, id);
-//     });
-// }
+    botx.action(/decrease_quantity_(.+)/, (ctx) => {
+        const itemId = ctx.match[1];
+        updateCartItemQuantity(ctx, userCarts, existingCarts, itemId, -1, bot);
+    });
 
-async function updateCartItemQuantity(ctx,userCarts, existingCarts, itemId, quantityChange) {
+    botx.action(/remove_item_(.+)/, (ctx) => {
+        removeCartItem(ctx, userCarts, existingCarts, id);
+    });
+
+    botx.action('previous_item', (ctx) => {
+        // Decrement the current item index and handle bounds
+        currentItemIndex = Math.max(0, currentItemIndex - 1);
+        editCart(ctx, userCarts, existingCarts, bot);
+    });
+
+    botx.action('next_item', (ctx) => {
+        // Increment the current item index and handle bounds
+        currentItemIndex = Math.min(itemsInCart.length - 1, currentItemIndex + 1);
+        editCart(ctx, userCarts, existingCarts, bot);
+    });
+}
+
+async function updateCartItemQuantity(ctx, userCarts, existingCarts, itemId, quantityChange, bot) {
     try {
         const userId = ctx.from.id;
         const userCart = userCarts.get(userId) || {};
@@ -153,9 +149,9 @@ async function updateCartItemQuantity(ctx,userCarts, existingCarts, itemId, quan
         // Update the existing cart
         existingCart[itemId] = newQuantityInExistingCart;
         existingCarts.set(userId, existingCart);
-        
+
         // Refresh the cart edit message
-        await editCart(ctx, userCarts, existingCarts);
+        await editCart(ctx, userCarts, existingCarts, bot);
     } catch (error) {
         console.error('Error updating cart item quantity:', error);
         ctx.reply('There was an error updating the cart item quantity. Please try again.');
@@ -179,13 +175,11 @@ async function removeCartItem(ctx, userCarts, existingCarts, itemId) {
         existingCarts.set(userId, existingCart);
 
         // Refresh the cart edit message
-        await editCart(ctx, userCarts, existingCarts);
+        await editCart(ctx, userCarts, existingCarts, bot);
     } catch (error) {
         console.error('Error removing cart item:', error);
         ctx.reply('There was an error removing the cart item. Please try again.');
     }
 }
 
-
-
-module.exports = { manageCart }
+module.exports = { manageCart };
